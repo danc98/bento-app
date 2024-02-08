8f1cbc1680e03c8b2a9f69ac02138482032cca9c
danc98 <d.cluxton@digi.jp>
2024-02-08 14:50:46 +0900

    More tests, readability improvements.

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -2,9 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\Models\Product;
 use App\Http\Controllers\ProductController;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
@@ -15,6 +15,34 @@ class ProductTest extends TestCase
     use RefreshDatabase;
 
     protected $seed = true;
+
+    /**
+     * Test that stock is correct for an item with only active labels.
+     */
+    public function test_product_stock_only_active_labels(): void {
+        // "Fish Box" has three active labels.
+        $fish_box = Product::findOrFail('6');
+        $this->assertEquals("3", $fish_box->stock());
+    }
+
+    /**
+     * Test that stock is zero for an inactive item.
+     */
+    public function test_product_stock_no_active_labels(): void {
+        // "Inactive" only has a single processed, non-active label.
+        $inactive_product = Product::findOrFail('8');
+        $this->assertEquals("0", $inactive_product->stock());
+    }
+
+    /**
+     * Test that stock is correct for an item with a mix of
+     * active/inactive labels.
+     */
+    public function test_product_stock_active_and_inactive_labels(): void {
+        // "Taco Box" has two active labels, and one printed/non-active label.
+        $taco_box = Product::findOrFail('1');
+        $this->assertEquals("2", $taco_box->stock());
+    }
 
     /**
      * Test that products pulled are properly formatted.
@@ -134,14 +162,14 @@ class ProductTest extends TestCase
     public function test_retrieve_product(): void {
         // Product to find.
         DB::table('products')->insert([
-            'id'        => '10',
-            'item_name' => 'Waldo',
-            'plu_cd' => '594539430',
-            'item_price' => '200',
-            'item_desc' => 'Found you!',
-            'item_img' => 'placeholder.png',
-            'created_at' => '2023-12-01 12:00:00',
-            'updated_at' => '2023-12-01 12:00:00',
+            'id'          => '10',
+            'item_name'   => 'Waldo',
+            'plu_cd'      => '594539430',
+            'item_price'  => '200',
+            'item_desc'   => 'Found you!',
+            'item_img'    => 'placeholder.png',
+            'created_at'  => '2023-12-01 12:00:00',
+            'updated_at'  => '2023-12-01 12:00:00',
             'category_id' => '5', // Corresponds to "Test" category.
         ]);
 
@@ -150,16 +178,16 @@ class ProductTest extends TestCase
 
         // Checking parameters.
         $this->assertNotEmpty($pulled_product);
-        $this->assertEquals("10", $pulled_product['product_id'], "product_id not correct.");
-        $this->assertEquals("Waldo", $pulled_product['name'], "item_name not correct.");
-        $this->assertEquals("594539430", $pulled_product['plu_cd'], "plu_cd not correct.");
-        $this->assertEquals("200", $pulled_product['price'], "item_price not correct.");
-        $this->assertEquals("Found you!", $pulled_product['desc'], "item_desc not correct.");
-        $this->assertEquals("placeholder.png", $pulled_product['img'], "item_img not correct.");
+        $this->assertEquals("10",                  $pulled_product['product_id'],   "product_id not correct.");
+        $this->assertEquals("Waldo",               $pulled_product['name'],         "item_name not correct.");
+        $this->assertEquals("594539430",           $pulled_product['plu_cd'],       "plu_cd not correct.");
+        $this->assertEquals("200",                 $pulled_product['price'],        "item_price not correct.");
+        $this->assertEquals("Found you!",          $pulled_product['desc'],         "item_desc not correct.");
+        $this->assertEquals("placeholder.png",     $pulled_product['img'],          "item_img not correct.");
         $this->assertEquals("2023-12-01 12:00:00", $pulled_product['ins_datetime'], "ins_datetime not correct.");
         $this->assertEquals("2023-12-01 12:00:00", $pulled_product['upd_datetime'], "upd_datetime not correct.");
-        $this->assertEquals("0", $pulled_product['stock'], "stock not correct."); // No active labels, so stock should be 0.
-        $this->assertEquals("Test", $pulled_product['category'], "category not correct.");
+        $this->assertEquals("0",                   $pulled_product['stock'],        "stock not correct."); // No active labels, so stock should be 0.
+        $this->assertEquals("Test",                $pulled_product['category'],     "category not correct.");
     }
 
     /**
@@ -246,8 +274,13 @@ class ProductTest extends TestCase
      * Test that a given product is properly stored.
      */
     public function test_store_product_with_no_image(): void {
+        $request = ['item_name'  => 'Apple',
+                    'plu_cd'     => '49504319234302',
+                    'item_price' => '444',
+                    'item_desc'  => 'An apple.',
+                    'category'   => 'Test'];
+
         // Send the request via the add route.
-        $request = ['item_name' => 'Apple', 'plu_cd' => '49504319234302', 'item_price' => '444', 'item_desc' => 'An apple.', 'category' => 'Test'];
         $this->followingRedirects()->post('/admin/products/add', $request)->assertStatus(200);
 
         $saved_product = DB::table('products')->where('item_name', 'Apple')->get();
@@ -256,12 +289,16 @@ class ProductTest extends TestCase
         // Checking parameters.
         $saved_product = $saved_product[0];
         $this->assertNotEmpty($saved_product);
-        $this->assertEquals("Apple", $saved_product->item_name, "item_name not correct.");
-        $this->assertEquals("49504319234302", $saved_product->plu_cd, "plu_cd not correct.");
-        $this->assertEquals("444", $saved_product->item_price, "item_price not correct.");
-        $this->assertEquals("An apple.", $saved_product->item_desc, "item_desc not correct.");
-        $this->assertEquals("placeholder.png", $saved_product->item_img, "item_img not correct.");
-        $this->assertEquals("5", $saved_product->category_id, "category_id not correct.");  // Corresponds to "Test" category.
+        $this->assertEquals("Apple",           $saved_product->item_name,   "item_name not correct.");
+        $this->assertEquals("49504319234302",  $saved_product->plu_cd,      "plu_cd not correct.");
+        $this->assertEquals("444",             $saved_product->item_price,  "item_price not correct.");
+        $this->assertEquals("An apple.",       $saved_product->item_desc,   "item_desc not correct.");
+        $this->assertEquals("placeholder.png", $saved_product->item_img,    "item_img not correct.");
+        $this->assertEquals("5",               $saved_product->category_id, "category_id not correct.");  // Corresponds to "Test" category.
+
+        // These parameters are set using execution time, so just check they are not empty.
+        $this->assertNotEmpty($saved_product->created_at);
+        $this->assertNotEmpty($saved_product->updated_at);
     }
 
     /**
@@ -271,9 +308,15 @@ class ProductTest extends TestCase
         // Creating a fake testing image.
         Storage::fake('public');
         $file = UploadedFile::fake()->image('apple.jpg');
+        
+        $request = ['item_name'  => 'Apple',
+                    'plu_cd'     => '49504319234302',
+                    'item_price' => '444',
+                    'item_desc'  => 'An apple.',
+                    'item_img'   => $file,
+                    'category'   => 'Test'];
 
         // Send the request via the add route.
-        $request = ['item_name' => 'Apple', 'plu_cd' => '49504319234302', 'item_price' => '444', 'item_desc' => 'An apple.', 'item_img' => $file, 'category' => 'Test'];
         $this->followingRedirects()->post('/admin/products/add', $request)->assertStatus(200);
 
         // Check that file was saved.
@@ -288,8 +331,14 @@ class ProductTest extends TestCase
         Storage::fake('public');
         $file = UploadedFile::fake()->image('apple.csv');
 
+        $request = ['item_name'  => 'Apple',
+                    'plu_cd'     => '49504319234302',
+                    'item_price' => '444',
+                    'item_desc'  => 'An apple.',
+                    'item_img'   => $file,
+                    'category'   => 'Test'];
+
         // Send the request via the add route.
-        $request = ['item_name' => 'Apple', 'plu_cd' => '49504319234302', 'item_price' => '444', 'item_desc' => 'An apple.', 'item_img' => $file, 'category' => 'Test'];
         $this->followingRedirects()->post('/admin/products/add', $request)->assertStatus(200);
 
         // Check that file was not saved saved.
@@ -300,8 +349,13 @@ class ProductTest extends TestCase
      * Test empty item_name during storage.
      */
     public function test_store_product_with_empty_item_name(): void {
+        $request = ['item_name'  => '',
+                    'plu_cd'     => '49504319234302',
+                    'item_price' => '444',
+                    'item_desc'  => 'An apple.',
+                    'category'   => 'Test'];
+
         // Send the request via the add route.
-        $request = ['item_name' => '', 'plu_cd' => '49504319234302', 'item_price' => '444', 'item_desc' => 'An apple.', 'category' => 'Test'];
         $response = $this->followingRedirects()->post('/admin/products/add', $request);
 
         $response->assertViewHas('errors');
@@ -313,8 +367,13 @@ class ProductTest extends TestCase
      * Test empty plu_cd during storage.
      */
     public function test_store_product_with_empty_plu_cd(): void {
+        $request = ['item_name'  => 'Apple',
+                    'plu_cd'     => '',
+                    'item_price' => '444',
+                    'item_desc'  => 'An apple.',
+                    'category'   => 'Test'];
+
         // Send the request via the add route.
-        $request = ['item_name' => 'Apple', 'plu_cd' => '', 'item_price' => '444', 'item_desc' => 'An apple.', 'category' => 'Test'];
         $response = $this->followingRedirects()->post('/admin/products/add', $request);
 
         $response->assertViewHas('errors');
@@ -326,8 +385,13 @@ class ProductTest extends TestCase
      * Test invalid PLU code during storage.
      */
     public function test_store_product_with_invalid_plu(): void {
+        $request = ['item_name'  => 'Apple', 
+                    'plu_cd'     => 'abc',
+                    'item_price' => '444',
+                    'item_desc'  => 'An apple.',
+                    'category'   => 'Test'];
+
         // Send the request via the add route.
-        $request = ['item_name' => 'Apple', 'plu_cd' => 'abc', 'item_price' => '444', 'item_desc' => 'An apple.', 'category' => 'Test'];
         $response = $this->followingRedirects()->post('/admin/products/add', $request);
 
         $response->assertViewHas('errors');
@@ -339,8 +403,13 @@ class ProductTest extends TestCase
      * Test empty price during storage.
      */
     public function test_store_product_with_empty_price(): void {
+        $request = ['item_name'  => 'Apple',
+                    'plu_cd'     => '49504319234302',
+                    'item_price' => '',
+                    'item_desc'  => 'An apple.',
+                    'category'   => 'Test'];
+
         // Send the request via the add route.
-        $request = ['item_name' => 'Apple', 'plu_cd' => '49504319234302', 'item_price' => '', 'item_desc' => 'An apple.', 'category' => 'Test'];
         $response = $this->followingRedirects()->post('/admin/products/add', $request);
 
         $response->assertViewHas('errors');
@@ -352,8 +421,13 @@ class ProductTest extends TestCase
      * Test invalid price during storage.
      */
     public function test_store_product_with_invalid_price(): void {
+        $request = ['item_name'  => 'Apple',
+                    'plu_cd'     => '49504319234302',
+                    'item_price' => 'abc',
+                    'item_desc'  => 'An apple.',
+                    'category'   => 'Test'];
+
         // Send the request via the add route.
-        $request = ['item_name' => 'Apple', 'plu_cd' => '49504319234302', 'item_price' => 'abc', 'item_desc' => 'An apple.', 'category' => 'Test'];
         $response = $this->followingRedirects()->post('/admin/products/add', $request);
 
         $response->assertViewHas('errors');
@@ -367,14 +441,14 @@ class ProductTest extends TestCase
     public function test_delete_product(): void {
         // Product to be deleted.
         DB::table('products')->insert([
-            'id'        => '10',
-            'item_name' => 'Target',
-            'plu_cd' => '594539430',
-            'item_price' => '200',
-            'item_desc' => 'Delete me!',
-            'item_img' => 'placeholder.png',
-            'created_at' => '2023-12-01 12:00:00',
-            'updated_at' => '2023-12-01 12:00:00',
+            'id'          => '10',
+            'item_name'   => 'Target',
+            'plu_cd'      => '594539430',
+            'item_price'  => '200',
+            'item_desc'   => 'Delete me!',
+            'item_img'    => 'placeholder.png',
+            'created_at'  => '2023-12-01 12:00:00',
+            'updated_at'  => '2023-12-01 12:00:00',
             'category_id' => '5',
         ]);
 
@@ -393,11 +467,65 @@ class ProductTest extends TestCase
     }
 
     /**
+     * Test that multiple products can be deleted at once.
+     */
+    public function test_delete_multiple_products(): void {
+        // Products to be deleted.
+        DB::table('products')->insert([
+            'id'          => '10',
+            'item_name'   => 'TargetOne',
+            'plu_cd'      => '594539430',
+            'item_price'  => '200',
+            'item_desc'   => 'Delete me!',
+            'item_img'    => 'placeholder.png',
+            'created_at'  => '2023-12-01 12:00:00',
+            'updated_at'  => '2023-12-01 12:00:00',
+            'category_id' => '5',
+        ]);
+
+        DB::table('products')->insert([
+            'id'          => '11',
+            'item_name'   => 'TargetTwo',
+            'plu_cd'      => '32040594',
+            'item_price'  => '200',
+            'item_desc'   => 'No, delete me!',
+            'item_img'    => 'placeholder.png',
+            'created_at'  => '2023-12-01 12:00:00',
+            'updated_at'  => '2023-12-01 12:00:00',
+            'category_id' => '5',
+        ]);
+
+        // Ensure they were saved.
+        $saved_product = DB::table('products')->where('id', '10')->get();
+        $this->assertNotEmpty($saved_product);
+
+        $saved_product = DB::table('products')->where('id', '11')->get();
+        $this->assertNotEmpty($saved_product);
+
+        // Send the request via the delete route.
+        $request = ['selected_products' => '10,11'];
+        $response = $this->followingRedirects()->delete('/admin/products/delete', $request);
+        $response->assertStatus(200);
+
+        // Ensure they were deleted.
+        $deleted_product = DB::table('products')->where('id', '10')->get();
+        $this->assertEmpty($deleted_product);
+
+        $deleted_product = DB::table('products')->where('id', '11')->get();
+        $this->assertEmpty($deleted_product);
+    }
+
+    /**
      * Test that a given product's category is flushed when orphaned.
      */
     public function test_flush_category(): void {
+        $request = ['item_name'  => 'Target',
+                    'plu_cd'     => '49504319234302',
+                    'item_price' => '444',
+                    'item_desc'  => 'Delete me!',
+                    'category'   => 'DeleteMe'];
+
         // Add request via add route to ensure new category is made.
-        $request = ['item_name' => 'Target', 'plu_cd' => '49504319234302', 'item_price' => '444', 'item_desc' => 'Delete me!', 'category' => 'DeleteMe'];
         $this->followingRedirects()->post('/admin/products/add', $request)->assertStatus(200);
 
         // Ensure the category exists.
